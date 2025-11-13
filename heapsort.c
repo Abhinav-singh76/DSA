8a54f8dccd38ea9052f9d10b061d2f5028f5eec9
Abhinav-singh76 <abhinavsingh5905109@gmail.com>
2025-11-13 22:26:08 +0530

    Add shiftdown function for heapsort implementation
    Implement the shiftdown function for heapsort.

---
+++ heapsort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void 
+void shiftdown(int a[],int n,int i){
+    int c;
+    int p=i;
+    while(p<n){
+        c=2*p+1;
+        if(c>=n) break;
+        if(c<n && a[c+1]>a[c]){
+            c=c+1;
+        }
+        if(a[p]<a[c]){void 
+void shiftdown(int a[],int n,int i){
+    int c;
+    int p=i;
+    while(p<n){
+        c=2*p+1;
+        if(c>=n) break;
+        if(c<n && a[c+1]>a[c]){
+            c=c+1;
+        }
+        if(a[p]<a[c]){
+            swap()
+        }
+    }
+}
+            swap()
+        }
+    }
+}
+int main()
+{
+    
+    return 0;
+}
